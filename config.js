3ddeee368f602d11eb469c4b7e3365cbbc3544b6
Gurmani Kaur <gurmanikaur3571@gmail.com>
2026-06-25 14:58:47 -0400

    Footer fix deleted

--- config.js
+++ config.js
@@ -34,17 +34,6 @@ document.querySelector("footer").innerHTML = `
   </div>
 `;
 
-document.querySelectorAll("#footer-root").forEach(el => {
-  el.innerHTML = `
-    <a href="./index.html#final-project">Work</a>
-    <a href="./about.html">About</a>
-
-    <div id="contact-info">
-      <a href="mailto:${config.student_email}">${config.student_email}</a>
-    </div>
-  `;
-});
-
 document.querySelectorAll('#student-name').forEach(el => {
   el.innerHTML = `${config.student_name}`;
 });
